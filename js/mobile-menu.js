f6bf42908378b230052e8a26c9cd7fd8b20ee0ed
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-05-16 09:08:34 +0300

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,36 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    mobileMenu: document.querySelector("[data-menu]"),
+  const mobileMenu = document.querySelector('[data-menu]');
+  const openMenuBtn = document.querySelector('[data-open-menu]');
+  const closeMenuBtn = document.querySelector('[data-close-menu]');
+  const body = document.body;
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    if (mobileMenu.classList.contains('is-open')) {
+      bodyLock();
+    } else {
+      bodyUnLock();
+    }
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-open");
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyUnLock();
+  });
+
+  function bodyLock() {
+    body.style.overflow = 'hidden';
+  }
+
+  function bodyUnLock() {
+    body.style.removeProperty('overflow');
   }
 })();
