407c684ac4d0f921b639ece121cdf83edff65553
Valentyna Taranchuk <valyataranchuk8@gmail.com>
2024-05-16 22:57:51 +0300

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,36 +1,18 @@
 (() => {
-  const mobileMenu = document.querySelector('[data-menu]');
-  const openMenuBtn = document.querySelector('[data-open-menu]');
-  const closeMenuBtn = document.querySelector('[data-close-menu]');
-  const body = document.body;
-
-  const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    if (mobileMenu.classList.contains('is-open')) {
-      bodyLock();
-    } else {
-      bodyUnLock();
-    }
+  const refs = {
+    // Додати атрибут data-menu-open на кнопку відкриття
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    // Додати атрибут data-menu-close на кнопку закриття
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    // Додати атрибут data-menu на мобільне меню
+    menu: document.querySelector("[data-menu]"),
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyUnLock();
-  });
-
-  function bodyLock() {
-    body.style.overflow = 'hidden';
-  }
-
-  function bodyUnLock() {
-    body.style.removeProperty('overflow');
+  function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на мобільне меню при натисканні на кнопки
+    refs.menu.classList.toggle("is-open");
   }
 })();
